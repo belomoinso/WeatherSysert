a1309ab2bdeee61ce17adb1bb648d496146d8d5f
Stanislav Belomoin <belomoinso@gmail.com>
2020-07-12 12:12:22 +0500

    Добавьте файлы проекта.

---
+++ WeatherSysert/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;   // Нужна, чтобы запускать внешние процессы
+using System.Net;           // Нужна, чтобы работать с Web
+using System.Threading;
+
+namespace YandexWeatherSysert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+            Console.Write("Сейчас в Сысерти {0}\u00B0C", getTemp(wc));
+
+
+        }
+
+        static string getTemp(WebClient w)
+        {
+            try
+            {
+                string html = w.DownloadString("https://yandex.ru/pogoda/sysert");
+                string sub = html.Substring(html.IndexOf("Текущая температура"));
+                sub = sub.Substring(sub.IndexOf("temp__value") + 13);
+                string temp = sub.Substring(0, sub.IndexOf("</span>"));
+
+                return temp;
+            }
+            catch (Exception)
+            {
+                return "Город не найден";
+            }
+
+
+        }
+
+    }
+}
+
